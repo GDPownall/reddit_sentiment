b5818f08804812ffaa0a368954c610e706a550e8
gpownall <gavin.pownall@cern.ch>
2021-03-04 15:48:12 +0000

    Bug fix: weights to GPU

--- src/model.py
+++ src/model.py
@@ -30,7 +30,7 @@ class AITAClassifier(nn.Module):
 def train(model, data, n_epochs=3, batch_size = 32):
     model = model.to(device)
     optimizer = AdamW(model.parameters(), lr=2e-5, correct_bias = False)
-    loss_fn = nn.CrossEntropyLoss(weight=torch.FloatTensor(data.one_hot_weights()))
+    loss_fn = nn.CrossEntropyLoss(weight=torch.FloatTensor(data.one_hot_weights()).to(device))
 
     data.train_test_split()
     train = data.split_data[0]
